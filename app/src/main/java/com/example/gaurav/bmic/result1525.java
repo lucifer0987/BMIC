70cd0609f65603a048c309ff4093eb1ef68840ea
Gaurav kumar <gk07071999@gmail.com>
2018-03-29 23:30:45 +0530

    removed function of download button

--- app/src/main/java/com/example/gaurav/bmic/result1525.java
+++ app/src/main/java/com/example/gaurav/bmic/result1525.java
@@ -2,30 +2,18 @@ package com.example.gaurav.bmic;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 public class result1525 extends AppCompatActivity {
 
