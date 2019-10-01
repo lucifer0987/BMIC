823e4dc0d66774bcda0e2000cd87087861867198
Gaurav Kumar <gaurv1407@gmail.com>
2019-10-02 01:13:42 +0530

    random update

--- app/src/main/java/com/example/gaurav/bmic/MainActivity.java
+++ app/src/main/java/com/example/gaurav/bmic/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.gaurav.bmic;
 
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.os.Handler;
-        import android.support.v7.app.AppCompatActivity;
-        import android.view.View;
-
-        import com.podcopic.animationlib.library.AnimationType;
-        import com.podcopic.animationlib.library.StartSmartAnimation;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import com.podcopic.animationlib.library.AnimationType;
+import com.podcopic.animationlib.library.StartSmartAnimation;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 5000;
